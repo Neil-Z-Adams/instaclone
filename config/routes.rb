b70ecd998beed70ada723981766a2877b6e31eb4
Neil Adams <neil.zhao.adams@gmail.com>
2023-12-04 19:38:43 -0800

    project updated

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+  devise_scope :user do
+    get '/users', to: 'devise/registration#new'
+    get '/users/password', to: 'devise/password#new'
+    get '/users/sign_out' => 'devise/session#destroy'
+  end
+  devise_for :users
+
+  get 'home/about'
+  get 'posts/myposts'
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "posts#index"
+end
